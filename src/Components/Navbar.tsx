c2c44c7fdf17bfb036ba32ef204458c3ffc746ef
Precious <Preshomondi@gmail.com>
2023-10-12 02:37:45 +0300

    Add semantic ui

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -16,46 +16,46 @@ function Navbar() {
   return (
     <div className="p-6">
       {/* Logo */}
-      <div className="logo flex w-full bg-red-600 items-center ">
-        <FontAwesomeIcon icon={faLink} className="text-2xl" />
-        <h3 className="text-2xl ml-4">Lovelink</h3>
+      <div className="logo flex w-full items-center cursor-pointer">
+        <FontAwesomeIcon icon={faLink} className="text-2xl bold" />
+        <h3 className="text-2xl ml-4 bold">Lovelink</h3>
       </div>
 
       {/* Navlinks */}
       <div className="nav-one mt-8 mb-10 border-b border-gray-200">
-        <div className="nav-three flex mb-4 items-center ">
+        <div className="nav-three flex mb-4 items-center  cursor-pointer">
           <FontAwesomeIcon icon={faHouse} className="text-secondary"/>
           <p className="ml-4 text-secondary">Discover</p>
         </div>
-        <div className="nav-three flex mb-4 items-center ">
-          <FontAwesomeIcon icon={faPeopleArrows} />
-          <p className="ml-4">Matches</p>
+        <div className="nav-three flex mb-4 items-center  cursor-pointer">
+          <FontAwesomeIcon icon={faPeopleArrows} className="text-texts" />
+          <p className="ml-4 text-texts font-semibold">Matches</p>
         </div>
-        <div className="nav-three flex mb-4 items-center ">
-          <FontAwesomeIcon icon={faMessage} />
-          <p className="ml-4">Chat Requests</p>
+        <div className="nav-three flex mb-4 items-center  cursor-pointer">
+          <FontAwesomeIcon icon={faMessage} className="text-texts" />
+          <p className="ml-4 text-texts font-semibold">Chat Requests</p>
         </div>
-        <div className="nav-three flex items-center mb-8">
-          <FontAwesomeIcon icon={faWallet} />
-          <p className="ml-4">Subscriptions</p>
+        <div className="nav-three flex items-center mb-8 cursor-pointer">
+          <FontAwesomeIcon icon={faWallet} className="text-texts" />
+          <p className="ml-4 text-texts font-semibold">Subscriptions</p>
         </div>
       </div>
 
       <div className="nav-two mb-8">
         <h4 className="mb-4 font-semibold text-zinc-700">Support</h4>
-        <div className="flex mb-4 items-center ">
-          <FontAwesomeIcon icon={faGear} />
-          <p className="ml-4">Settings</p>
+        <div className="flex mb-4 items-center cursor-pointer">
+          <FontAwesomeIcon icon={faGear} className="text-texts" />
+          <p className="ml-4 text-texts font-semibold">Settings</p>
         </div>
-        <div className="flex mb-4 items-center ">
-          <FontAwesomeIcon icon={faCircleInfo} />
-          <p className="ml-4">Help</p>
+        <div className="flex mb-4 items-center cursor-pointer">
+          <FontAwesomeIcon icon={faCircleInfo} className="text-texts" />
+          <p className="ml-4 text-texts font-semibold">Help</p>
         </div>
       </div>
 
-      <div className="nav-three flex items-center mt-6">
-        <FontAwesomeIcon icon={faRightFromBracket} />
-        <p className="ml-4">Logout</p>
+      <div className="nav-three flex items-center mt-6 cursor-pointer">
+        <FontAwesomeIcon icon={faRightFromBracket} className="text-texts" />
+        <p className="ml-4 text-texts font-semibold">Logout</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         mainsection: '#F7F8FC',
         primary: '#F2481D',
         secondary: '#F15073',
+        texts: '#A8A8A8',
         lighttext: '#FDEEF1',
         lightblack: '#1D1D1D',
         dark: '#101010',
