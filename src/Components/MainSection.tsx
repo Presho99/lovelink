bdc2fe7e2bae73885168fe9041ff8d56363ce3b1
Precious <Preshomondi@gmail.com>
2023-10-13 19:33:44 +0300

    Add components

--- src/Components/MainSection.tsx
+++ src/Components/MainSection.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Profile } from "../types/types";
 
-function MainSection() {
-  return (
-    <div>MainSection</div>
-  )
+interface MainSectionProps {
+  profiles: Profile[];
 }
 
-export default MainSection
+function MainSection({ profiles }: MainSectionProps) {
+  console.log(profiles);
+
+  if (!Array.isArray(profiles)) {
+    return <div>No profiles available.</div>;
+  }
+
+  return (
+    <div className="p-6 h-full">
+      <div className="flex h-1/8">
+        {/* search bar */}
+        <div className="ui search w-4/5">
+          <input
+            className="prompt"
+            type="text"
+            placeholder="Search username..."
+          />
+        </div>
+        {/* upgrade button */}
+        <button className="w-1/5 bg-secondary rounded-full text-white">
+          Upgrade Now
+        </button>
+      </div>
+
+      {/* profiles */}
+      <div className="flex h-4/5 mt-6 bg-red-400 flex-wrap justify-center ">
+        {profiles.map((person) => (
+          <div
+            key={person.id}
+            className="w-60 h-60 m-2 relative rounded-md p-2"
+            style={{ backgroundImage: `url(${person.avatar})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
+          >
+            <div className="absolute">
+              <h2>{person.name}</h2>
+              <p>{person.bio}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
